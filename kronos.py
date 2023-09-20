b1fd60fd2c90f3e2bf85c99cb6515a6768275b3a
Michael <30883926+mgalde@users.noreply.github.com>
2023-09-20 14:24:33 -0700

    Update kronos.py

--- kronos.py
+++ kronos.py
@@ -10,30 +10,42 @@ class KronosApp:
         self.root.bind("<Escape>", self.exit_app)
         self.root.bind("<Up>", self.select_option_up)
         self.root.bind("<Down>", self.select_option_down)
+        self.root.config(cursor="none")  # Hide the mouse cursor
         
         self.password_entry = tk.Entry(self.root, font=("Impact", 20, "bold"), fg="#192e2a", bg="#f2fcfa", justify="center")
         self.password_entry.insert(0, "PASSWORD")
         self.password_entry.bind("<FocusIn>", self.on_focus_in)
+        self.password_entry.bind("<Key>", self.on_key_press)
         self.password_entry.bind("<Return>", self.check_password)
         self.password_entry.place(relx=0.5, rely=0.5, anchor="center", width=self.root.winfo_screenwidth())
+        self.password_entry.focus_set()  # Automatically focus the password entry
         
         self.options = ["ISLAND OPERATIONS", "FINANCES", "OMNIDROID METATRINING", "SUPERS"]
         self.selected_option = 0
         self.option_labels = []
 
     def on_focus_in(self, event):
-        self.password_entry.delete(0, "end")
+        if self.password_entry.get() == "PASSWORD":
+            self.password_entry.delete(0, "end")
+
+    def on_key_press(self, event):
+        if self.password_entry.get() == "PASSWORD":
+            self.password_entry.delete(0, "end")
 
     def check_password(self, event):
         if self.password_entry.get() == "KRONOS":
             self.password_entry.destroy()
+            self.border = tk.Frame(self.root, bg="#f2fcfa", bd=10)
+            self.border.place(relx=0.5, rely=0.5, anchor="center", width=self.root.winfo_screenwidth(), height=320)
             self.display_options()
         else:
             self.password_entry.delete(0, "end")
             self.password_entry.insert(0, "INCORRECT")
+            self.password_entry.config(fg="#D6294A")
             self.root.after(5000, self.reset_password_entry)
 
     def reset_password_entry(self):
+        self.password_entry.config(fg="#192e2a")
         self.password_entry.delete(0, "end")
         self.password_entry.insert(0, "PASSWORD")
 
